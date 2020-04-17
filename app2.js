51fb79d919e781f8549db14c02543088bb9fe32a
Kush Daga <kushdaga@hotmail.com>
2020-04-17 19:51:48 +0530

    made ui changes in animations

--- app2.js
+++ app2.js
@@ -1,143 +1,93 @@
 const flightPath = {
-    curviness: 1.55,
+    curviness: 0.2,
     autoRotate: false,
     values: [
         { x: 0, y: 100 },
         {
             x: 300,
-            y: -10
+            y: -10,
         },
         {
             x: 500,
-            y: -40
+            y: -40,
         },
         {
             x: 700,
-            y: -80
+            y: -80,
         },
         {
             x: 680,
-            y: -100
+            y: -100,
+        },
+
+        {
+            x: 700,
+            y: -160,
         },
         {
             x: 660,
-            y: -120
+            y: -200,
+        },
+        {
+            x: 640,
+            y: -250,
         },
         {
             x: 600,
-            y: -160
+            y: -300,
         },
         {
             x: 300,
-            y: -320
+            y: -400,
         },
         {
             x: 100,
-            y: -640
+            y: -640,
         },
         {
             x: 10,
-            y: -1000
-        }
-    ]
+            y: -1000,
+        },
+    ],
 };
-function onScroll() {
-    var text = document.getElementById("test");
-    var lol = document.getElementById("animationObject");
-    var position = lol.getBoundingClientRect();
-    var x = position.left;
-    var y = position.top;
-    x = Math.floor(x);
-    y = Math.floor(y);
-    text.textContent = x + "/" + y;
-}
-window.addEventListener("scroll", onScroll);
+// function onScroll() {
+//     var text = document.getElementById("test");
+//     var lol = document.getElementById("animationObject");
+//     var position = lol.getBoundingClientRect();
+//     var x = position.left;
+//     var y = position.top;
+//     x = Math.floor(x);
+//     y = Math.floor(y);
+//     text.textContent = x + "/" + y;
+// }
+var fromSetting = {
+    ease: Power1.easeInOut,
+    zIndex: 2,
+    opacity: 0,
+};
+var toSetting = {
+    bezier: flightPath,
+    fontSize: "0px",
+    zIndex: -2,
+    opacity: 6,
+};
+// window.addEventListener("scroll", onScroll);
 const tween = new TimelineLite();
 tween
-    .fromTo(
-        "#animationObject",
-        1,
-        {
-            ease: Power1.easeInOut,
-            x: 10,
-            y: 200,
-            height: "100px"
-        },
-        {
-            bezier: flightPath,
-            height: "0px"
-        }
-    )
-    .fromTo(
-        "#animationObject2",
-        1,
-        {
-            ease: Power1.easeInOut,
-            x: 10,
-            y: 200,
-            height: "100px"
-        },
-        {
-            bezier: flightPath,
-            height: "0px"
-        },
-        "-=0.5"
-    )
-    .fromTo(
-        "#animationObject3",
-        1,
-        {
-            ease: Power1.easeInOut,
-            x: 10,
-            y: 200,
-            height: "100px"
-        },
-        {
-            bezier: flightPath,
-            height: "0px"
-        },
-        "-=0.5"
-    )
-    .fromTo(
-        "#animationObject4",
-        1,
-        {
-            ease: Power1.easeInOut,
-            x: 10,
-            y: 200,
-            height: "100px"
-        },
-        {
-            bezier: flightPath,
-            height: "0px"
-        },
-        "-=0.5"
-    )
-    .fromTo(
-        "#animationObject5",
-        1,
-        {
-            ease: Power1.easeInOut,
-            x: 10,
-            y: 200,
-            height: "100px"
-        },
-        {
-            bezier: flightPath,
-            height: "0px"
-        },
-        "-=0.5"
-    );
+    .fromTo("#animationObject", 2, fromSetting, toSetting)
+    .fromTo("#animationObject2", 2, fromSetting, toSetting, "-=1")
+    .fromTo("#animationObject3", 2, fromSetting, toSetting, "-=1")
+    .fromTo("#animationObject4", 2, fromSetting, toSetting, "-=1")
+    .fromTo("#animationObject5", 2, fromSetting, toSetting, "-=1");
 
 const controller = new ScrollMagic.Controller();
 const sceneScroll = new ScrollMagic.Scene({
     triggerElement: ".animation",
     duration: 4000,
-    triggerHook: 0
+    triggerHook: 0,
 })
     .setTween(tween)
     .setPin(".animation")
-    .addIndicators()
     .addTo(controller);
 
 //variables for setup
@@ -159,7 +109,7 @@ function init() {
     const far = 500; //far clipping distance
     //CAMERA
     cameraPizzaSlice = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    cameraPizzaSlice.position.set(0, 0, 50);
+    cameraPizzaSlice.position.set(0, 0, 45);
 
     const ambientPizzaSlice = new THREE.AmbientLight(0x404040, 2);
     scenePizzaSlice.add(ambientPizzaSlice);
@@ -170,7 +120,7 @@ function init() {
     //RENDERER
     rendererPizzaSlice = new THREE.WebGLRenderer({
         antialias: true,
-        alpha: true
+        alpha: true,
     });
     rendererPizzaSlice.setSize(
         containerPizzaSlice.clientWidth,
@@ -180,7 +130,7 @@ function init() {
     containerPizzaSlice.appendChild(rendererPizzaSlice.domElement);
     //Load Model
     let loader = new THREE.GLTFLoader();
-    loader.load("./pizza_slice/scene.gltf", function(gltf) {
+    loader.load("./pizza_slice/scene.gltf", function (gltf) {
         scenePizzaSlice.add(gltf.scene);
         pizzaSlice = gltf.scene.children[0];
         animate();
@@ -204,13 +154,14 @@ window.addEventListener("resize", onWindowResizeSlice);
 var progress;
 var positionY = 0;
 function animate() {
-    sceneScroll.on("progress", function(event) {
+    sceneScroll.on("progress", function (event) {
         // console.log("Scene progress changed to " + event.progress);
         progress = event.progress;
     });
     var state = sceneScroll.state();
     if (state === "DURING") {
-        pizzaSlice.rotation.y = progress * Math.PI;
+        pizzaSlice.rotation.y = progress * Math.PI * 2;
+        cameraPizzaSlice.position.set(0, 0, 55 - progress * 30);
     }
     pizzaSlice.rotation.x = -0.05;
 
